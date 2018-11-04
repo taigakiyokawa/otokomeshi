6383c3f8dd3fad142a9dc223ed88a07f93650cf0
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-04 13:49:00 +0900

    post-user間の関連付けをクリア

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,6 @@ class Post < ApplicationRecord
   mount_uploader :meshim, ImageUploader
   validates :title, presence: true
   validates :meshim, presence: true
-
-  class Post < ApplicationRecord
-    belongs_to :user
-    has_many :likes, dependent: :destroy
-  end
+  
+  has_many :likes, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_090731) do
+ActiveRecord::Schema.define(version: 2018_11_04_043803) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
