267be813aaf32398374af3e34861874a5cf0d4cc
Taiga KIYOKAWA <taiga.kiyokawa106@mail.kyutech.jp>
2018-11-05 15:29:34 +0900

    title12文字以内，body40文字以内にバリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   mount_uploader :meshim, ImageUploader
-  validates :title, presence: true
+  validates :title, {presence: true, length: {maximum: 12}}
   validates :meshim, presence: true
+  validates :body, {length: {maximum: 40}}
   
   has_many :likes, dependent: :destroy
   belongs_to :user
