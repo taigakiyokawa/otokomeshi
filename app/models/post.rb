f1eab323709dc039a8de72fea696026df003a3ab
sono <sono428@docomo.ne.jp>
2018-11-05 19:54:50 +0900

    検索フォームに全投稿表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ApplicationRecord
   
   has_many :likes, dependent: :destroy
   belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   def self.search(search) #self.でクラスメソッドとしている
     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
-      User.where(['email LIKE ?', "%#{search}%"])
+      User.where(['name LIKE ?', "%#{search}%"])
     else
       User.all #全て表示。
     end
