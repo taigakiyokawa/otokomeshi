f9f86f2caf8f6fdaf30dd360c7135c70c08656aa
Taiga KIYOKAWA <taiga.kiyokawa106@mail.kyutech.jp>
2018-11-06 15:44:04 +0900

    天晴数表示と削除ボタンをいい感じにした

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,12 @@
 ActiveRecord::Schema.define(version: 2018_11_06_023721) do
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
