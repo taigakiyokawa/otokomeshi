d3d284ba12852af454b6f177a78dc44fd2f9bf0a
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-06 11:57:41 +0900

    天晴機能再実装(カウント表示のため)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts do
-    resources :likes, only: [:create,:destroy]
-  end
+  resources :posts
+
+  post   '/like/:post_id' => 'likes#like',   as: 'like'
+  delete '/like/:post_id' => 'likes#unlike', as: 'unlike'
+
   devise_for :users
   get "/" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_143451) do
+ActiveRecord::Schema.define(version: 2018_11_06_023721) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
