f8261faae57c4477155412347b0c4c367b31acd6
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-07 13:22:24 +0900

    総天晴数の集計をhelperに記述

--- app/helpers/shogos_helper.rb
+++ app/helpers/shogos_helper.rb
@@ -1,2 +1,12 @@
 module ShogosHelper
+    def get_likes_total(user)
+        x = 0
+        @posts = Post.where(user_id: user.id)
+        @posts.each do |post|
+          x += post.likes.count
+        end
+        return x
+    end
+
+    
 end
