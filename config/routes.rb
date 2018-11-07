22d3ea9c9bbee6cd91fda176b1343afb28531e86
Taiga KIYOKAWA <taiga.kiyokawa106@mail.kyutech.jp>
2018-11-07 13:13:46 +0900

    users_controllerを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   resources :posts
 
   post   '/like/:post_id' => 'likes#like',   as: 'like'
   delete '/like/:post_id' => 'likes#unlike', as: 'unlike'
 
   devise_for :users
+  resources :users, :only => [:index, :show]
   get "/" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
