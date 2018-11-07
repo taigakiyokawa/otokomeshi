d1adeb4c99cbd26cc8dd0a67a979d71337483bfa
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-07 13:49:16 +0900

    称号plan1: if分岐で表示させる（カラムとして保存はしない）

--- app/helpers/shogos_helper.rb
+++ app/helpers/shogos_helper.rb
@@ -19,4 +19,26 @@ module ShogosHelper
         end
         return likes_total
     end
+
+    def show_shogo_2(user)
+        likes_total = get_likes_total(user)
+
+        if shogo = user.shogos.find_by(user_id: user.id)
+            if not shogo.status == likes_total
+                @shogo = Shogo.update user_id: user.id, status: likes_total
+            end
+        else
+            @shogo = Shogo.create user_id: user.id, status: likes_total
+        end
+
+        if likes_total < 1
+            return "見習い"
+        elsif likes_total < 2
+            return "新参者"
+        elsif likes_total < 3
+            return "中級者"
+        else
+            return "横綱"
+        end
+    end
 end
