3e7f644cf5d009296d577f0a66925a5fff0b10e6
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-07 13:30:23 +0900

    関数化してviewがスッキリ！

--- app/helpers/shogos_helper.rb
+++ app/helpers/shogos_helper.rb
@@ -8,5 +8,15 @@ module ShogosHelper
         return x
     end
 
-    
+    def show_shogo(user)
+        likes_total = get_likes_total(user)
+        if shogo = user.shogos.find_by(user_id: user.id)
+            if not shogo.status == likes_total
+                @shogo = Shogo.update user_id: user.id, status: likes_total
+            end
+        else
+            @shogo = Shogo.create user_id: user.id, status: likes_total
+        end
+        return likes_total
+    end
 end
