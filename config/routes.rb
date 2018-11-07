a7314a7291dbab9fb2ea4f12234ed8f9484d2922
Taiga KIYOKAWA <taiga.kiyokawa106@mail.kyutech.jp>
2018-11-08 03:05:41 +0900

    destroy user_controller and chande detail for rankIndex

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   delete '/like/:post_id' => 'likes#unlike', as: 'unlike'
 
   devise_for :users
-  get 'users' => "users#index"
-  get 'users/:id' => "users#show"
+
   get "/" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
