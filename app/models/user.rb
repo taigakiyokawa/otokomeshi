caeeadb3825317ca2d5031a1d3565105308746c6
Taiga KIYOKAWA <taiga.kiyokawa106@mail.kyutech.jp>
2018-11-08 11:46:28 +0900

    ユーザー名とパスワードのみで入門/ログイン完了ぅ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:username]
   
   validates :username, {presence: true, uniqueness: true, length: {maximum: 16}}
 
