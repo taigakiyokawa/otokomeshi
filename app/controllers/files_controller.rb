0b907b4e8325b70d0332c055f07f84e216bb8a70
naoto67 <uver.com67@gmail.com>
2018-11-08 20:13:03 +0900

    add comments.

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -2,8 +2,10 @@ class FilesController < ApplicationController
   before_action :authenticate_user!
   
   def create
+    # 現在のユーザの画像をアップデート
     @user = current_user
     if @user.update_attributes(user_img: params[:user_img])
+      
     end
   end
 
