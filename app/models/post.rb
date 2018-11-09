71da4ceb4455da537802aad9b25b14b0e89a57ff
Taiga KIYOKAWA <taiga.kiyokawa106@mail.kyutech.jp>
2018-11-09 23:11:16 +0900

    meshi_uploader作成&フォームをいい感じにする途中

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ApplicationRecord
-  mount_uploader :meshim, ImageUploader
+  mount_uploader :meshim, MeshiUploader
   validates :title, {presence: true, length: {maximum: 12}}
   validates :meshim, presence: true
   validates :body, {length: {maximum: 40}}
   
   has_many :likes, dependent: :destroy
   belongs_to :user
+  
 end
