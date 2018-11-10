d96488559fadd05d175298a43149b6ed988e3a7b
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-11 08:27:49 +0900

    shogo-firstwo修正

--- app/helpers/shogo_first_helper.rb
+++ app/helpers/shogo_first_helper.rb
@@ -1,24 +1,16 @@
 module ShogoFirstHelper
-    def get_likes_total(user)
-        x = 0
-        @posts = Post.where(user_id: user.id)
-        @posts.each do |post|
-          x += post.likes.count
-        end
-        return x
-    end
 
     def show_shogo_first(user)
-        likes_total = get_likes_total(user)
+        posts_total = Post.where(user_id: user.id).count
 
-        new_first_name = get_shogo_first_name(likes_total)
+        new_first_name = get_shogo_first_name(posts_total)
 
         if now_status = ShogoFirst.where(user_id: user.id).maximum(:status)
-            if now_status < likes_total
-                @shogo_first = ShogoFirst.create(user_id: user.id, status: likes_total, shogo_first_name: new_first_name)
+            if now_status < posts_total
+                @shogo_first = ShogoFirst.create(user_id: user.id, status: posts_total, shogo_first_name: new_first_name)
             end
         else
-            @shogo_first = ShogoFirst.create(user_id: user.id, status: likes_total, shogo_first_name: new_first_name)
+            @shogo_first = ShogoFirst.create(user_id: user.id, status: posts_total, shogo_first_name: new_first_name)
         end
 
         return new_first_name
