f3e3efdfcf8f869e685977d7556e29f3b30c34db
Taiga KIYOKAWA <taiga.kiyokawa106@mail.kyutech.jp>
2018-11-13 22:21:13 +0900

    users_controllerを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-<<<<<<< HEAD
   
-=======
->>>>>>> 5559c2983039e1260a0c12b017e96a90f93a9559
   def index
     @users = User.all.order(created_at: :desc)
   end
