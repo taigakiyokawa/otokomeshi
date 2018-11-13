6df880108319bedd7f9e65cba81f082fa5e875b7
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-13 16:34:45 +0900

    Userの持つshogo_firtst(/last)_idのデフォルト値設定

--- db/migrate/20181113063752_add_shogo_first_id_to_users.rb
+++ db/migrate/20181113063752_add_shogo_first_id_to_users.rb
@@ -1,5 +1,5 @@
 class AddShogoFirstIdToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :shogo_first_id, :integer
+    add_column :users, :shogo_first_id, :integer, default: 0
   end
 end
--- db/migrate/20181113063817_add_shogo_last_id_to_users.rb
+++ db/migrate/20181113063817_add_shogo_last_id_to_users.rb
@@ -1,5 +1,5 @@
 class AddShogoLastIdToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :shogo_last_id, :integer
+    add_column :users, :shogo_last_id, :integer, default: 0
   end
 end
