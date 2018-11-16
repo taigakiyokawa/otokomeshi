3121d84149adb42ff77304482f03aa4b33370165
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-16 17:54:24 +0900

    DB再設計後の称号実装

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,32 +1,79 @@
 module UsersHelper
 
     def set_shogo_first(user)
-        post_count = Post.where(user_id: user.id).count
+        posts_count = Post.where(user_id: user.id).count
 
-        if user.shogo_first_id < post_count
-            if post_count < 2
-                return 1
-            elsif post_count < 5
-                return 2
-            elsif post_count < 7
-                return 3
-            elsif post_count < 10
-                return 4
-            elsif post_count < 15
-                return 5
-            elsif post_count < 18
-                return 6
-            elsif post_count < 22
-                return 7
-            elsif post_count < 26
-                return 8
-            elsif post_count < 35
-                return 9
-            else
-                return 10
-            end
+        new_id = 1
+
+        if posts_count < 2
+            new_id =  1
+        elsif posts_count < 5
+            new_id = 2
+        elsif posts_count < 7
+            new_id = 3
+        elsif posts_count < 10
+            new_id = 4
+        elsif posts_count < 15
+            new_id = 5
+        elsif posts_count < 18
+            new_id = 6
+        elsif posts_count < 22
+            new_id = 7
+        elsif posts_count < 26
+            new_id = 8
+        elsif posts_count < 35
+            new_id = 9
+        else
+            new_id =10
+        end
+        
+        if user.shogo_first_id < new_id
+            return new_id
         else
             return user.shogo_first_id
+        end
+    end
+
+    def get_likes_total(user)
+        x = 0
+        @posts = Post.where(user_id: user.id)
+        @posts.each do |post|
+          x += post.likes.count
+        end
+        return x
+    end
+    
+    def set_shogo_last(user)
+        likes_total = get_likes_total(user)
+
+        new_id = 1
+
+        if likes_total < 2
+            new_id =  1
+        elsif likes_total < 5
+            new_id = 2
+        elsif likes_total < 7
+            new_id = 3
+        elsif likes_total < 10
+            new_id = 4
+        elsif likes_total < 13
+            new_id = 5
+        elsif likes_total < 16
+            new_id = 6
+        elsif likes_total < 20
+            new_id = 7
+        elsif likes_total < 24
+            new_id = 8
+        elsif likes_total < 30
+            new_id = 9
+        else
+            new_id =10
+        end
+
+        if user.shogo_last_id < new_id
+            return new_id
+        else
+            return user.shogo_last_id
         end
     end   
 end
--- db/migrate/20181113075915_set_default_to_user.rb
+++
@@ -1,6 +0,0 @@
-class SetDefaultToUser < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :shogo_first_id, :integer, :default => 0
-    change_column :users, :shogo_last_id, :integer, :default => 0
-  end
-end
