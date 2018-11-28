d38ff87a2d8b8a25d4c512dc4bc4f2a2b230735f
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-28 11:10:15 +0900

    いらない関数を削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,3 @@
 class Task < ApplicationRecord
     has_many :posts
-
-    def self.post_to_this
-        post = Post.new(
-            title: " ",
-            meshim: " ",
-            task_id: self.id)
-        
-    end
 end
