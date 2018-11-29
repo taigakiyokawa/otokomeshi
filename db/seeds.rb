cfcd604fd0f3b46a7488405d8f4c0cce9550684e
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-11-29 22:48:43 +0900

    csvファイルの名前変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,11 +25,11 @@ CSV.foreach('db/user.csv') do |us|
     )
  end
 
-CSV.foreach('db/shogo_first_list.csv') do |sf|
+CSV.foreach('db/shogo_first.csv') do |sf|
    ShogoFirst.create(name: sf[0])
 end
 
-CSV.foreach('db/shogo_last_list.csv') do |sl|
+CSV.foreach('db/shogo_last.csv') do |sl|
    ShogoLast.create(name: sl[0])
 end
 
