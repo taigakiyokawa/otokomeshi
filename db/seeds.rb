525abd4ee57d404e8da72d853e3a6ca4b3138dde
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-12-01 22:56:20 +0900

    seedをshogo, taskのみにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,28 @@
 
 require "csv"
 
-CSV.foreach('db/user.csv') do |us|
-    User.create(
-        username: us[0], 
-        user_img: Rails.root.join(us[1]).open,
-        password: "a")
- end
+# CSV.foreach('db/user.csv') do |us|
+#     User.create(
+#         username: us[0], 
+#         user_img: Rails.root.join(us[1]).open,
+#         password: "a")
+#  end
 
- CSV.foreach('db/post.csv') do |ps|
-    Post.create(
-        title: ps[0],
-        meshim: Rails.root.join(ps[1]).open,
-        body: ps[2],
-        user_id: ps[3],
-        task_id: ps[4])
- end
+#  CSV.foreach('db/post.csv') do |ps|
+#     Post.create(
+#         title: ps[0],
+#         meshim: Rails.root.join(ps[1]).open,
+#         body: ps[2],
+#         user_id: ps[3],
+#         task_id: ps[4])
+#  end
 
- CSV.foreach('db/like.csv') do |lk|
-    Like.create(
-        post_id: lk[0],
-        user_id: lk[1]
-    )
- end
+#  CSV.foreach('db/like.csv') do |lk|
+#     Like.create(
+#         post_id: lk[0],
+#         user_id: lk[1]
+#     )
+#  end
 
 CSV.foreach('db/shogo_first.csv') do |sf|
    ShogoFirst.create(name: sf[0])
