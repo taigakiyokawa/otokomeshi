dd745308e43b6f4fab9d9889492f53ed9a4b7bfc
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-12-03 17:54:15 +0900

    称号の自動更新をナシにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,18 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @shogo_first = ShogoFirst.find_by(id: set_shogo_first(@user))
-    @shogo_last = ShogoLast.find_by(id: set_shogo_last(@user))
+
+    @shogo_first = ShogoFirst.find_by(id: @user.shogo_first_id)
+    @shogo_last = ShogoLast.find_by(id: @user.shogo_first_id)
     @shogo_first_ex = ShogoFirstEx.find_by(id: @user.shogo_first_ex_id)
     @shogo_last_ex = ShogoLastEx.find_by(id: @user.shogo_last_ex_id)
+
     @like_posts = Post.where(id: @user.likes.map(&:post_id)).search(params[:search]).order(created_at: :desc)
     @user_posts = Post.where(user_id: @user.id).order(created_at: :desc)
+    
     @user_shogo_firsts = ShogoFirst.where("id <= ?", @shogo_first.id)
     @user_shogo_lasts = ShogoLast.where("id <= ?", @shogo_last.id)
+    
     shogo_ex_ids = get_shogo_ex(@user)
     @shogo_first_ex_list = ShogoFirstEx.where(id: shogo_ex_ids)
     @shogo_last_ex_list = ShogoLastEx.where(id: shogo_ex_ids)
