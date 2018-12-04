cce3a2e188f078127aa523c1a155b4608df7650a
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-12-04 19:43:54 +0900

    デフォルトのnoticeを削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -93,7 +93,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -107,7 +107,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
