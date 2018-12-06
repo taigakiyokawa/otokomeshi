898f9796d295d66e381efa767cfd33bd59bad3e2
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-12-06 16:12:11 +0900

    regionを東京に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,11 +4,11 @@ CarrierWave.configure do |config|
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['ACCESS_KEY_ID'],  # required
       aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],     # required
-      region:                'us-east-1',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
+      region:                'ap-northeast-1',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
       #host:                  's3.example.com',             # optional, defaults to nil
       #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
     }
-    config.fog_directory  = 'otoko-meshi-z'                # required
+    config.fog_directory  = 'otoko-meshi-test'                # required
     config.fog_public     = false                                                 # optional, defaults to true
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   end
