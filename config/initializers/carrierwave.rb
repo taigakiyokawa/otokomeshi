e35081d0ba45f9115722ae446e3a753cc74163cd
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2018-12-06 16:50:21 +0900

    endpointを設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       region:                'ap-northeast-1',
       # region:                'us-east-1', # optional, defaults to 'us-east-1' オハイオ＝us-east-2
       #host:                  's3.example.com',             # optional, defaults to nil
-      #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+      endpoint:              'https://s3.ap-northeast-1.amazonaws.com:8080' # optional, defaults to nil
     }
     config.fog_directory  = 'otoko-meshi-test'                # required
     config.fog_public     = false                                                 # optional, defaults to true
